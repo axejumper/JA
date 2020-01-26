490c85210eb10f3f27eb143a9b6c0fc02fb31957
jumperbest <jumperokbest@gmail.com>
2020-01-26 23:39:37 +0200

    Remove console.

---
+++ index.js
@@ -0,0 +1,7 @@
+import Parallax from 'parallax-js';
+import x from './bubbles.js'; 
+
+console.log(Parallax);
+
+var scene = document.querySelector('.parallax_js');
+var parallaxInstance = new Parallax(scene);
---
+++ photo-controller.js
@@ -0,0 +1,5 @@
+let aplusb = (a,b) => a+b;
+
+let pcV11 = aplusb("axe","jumper");
+
+export default pcV11;
